f95612102a6c7b40c5a9a7f81f5bb171baf334fa
EpicBlade <51732350+jaydeep-dev@users.noreply.github.com>
2024-02-01 17:15:50 -0500

    Movement Completed

---
+++ Inverse_Unity/Assets/Tests/Jaydeep/Scripts/Player/MyPlayerInput.cs
@@ -0,0 +1,51 @@
+using Minimilist.Player.PlayerActions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Minimalist.Player
+{
+    public class MyPlayerInput : MonoBehaviour
+    {
+        public static MyPlayerInput Instance { get; set; }
+
+        // Public Fields
+        public float MoveVector { get; private set; }
+        public bool IsJumped { get; private set; }
+
+        // Private Fields
+        private PlayerControls _inputs;
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                DestroyImmediate(Instance.gameObject);
+
+            _inputs = new PlayerControls();
+        }
+
+        #region Input Events
+
+        private void OnEnable()
+        {
+            _inputs.Enable();
+            _inputs.Player.Move.performed += ctx => MoveVector = ctx.ReadValue<float>();
+            _inputs.Player.Move.canceled += ctx => MoveVector = 0f;
+        }
+
+        private void OnDisable()
+        {
+            _inputs.Disable();
+        }
+        #endregion
+
+        private void Update()
+        {
+            IsJumped = _inputs.Player.Jump.WasPerformedThisFrame();
+        }
+    }
+}
