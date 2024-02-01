a8c824a87b3cb34d44dd3c45e8794a676cda0f59
EpicBlade <51732350+jaydeep-dev@users.noreply.github.com>
2024-02-01 17:19:21 -0500

    Dog Pet Added

---
+++ Inverse_Unity/Assets/Tests/Jaydeep/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minimilist.Enemies
+{
+    public class Enemy : MonoBehaviour
+    {
+        // Just to detect enemy for now
+    }
+}
