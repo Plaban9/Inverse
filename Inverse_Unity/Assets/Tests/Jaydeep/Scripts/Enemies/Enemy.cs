3333a1f7e9c1c6d0e6f75841003383de845c1dd4
EpicBlade <51732350+jaydeep-dev@users.noreply.github.com>
2024-02-12 19:08:56 -0500

    Enemy State Machine Implemented

--- Inverse_Unity/Assets/Tests/Jaydeep/Scripts/Enemies/Enemy.cs
+++ Inverse_Unity/Assets/Tests/Jaydeep/Scripts/Enemies/Enemy.cs
@@ -1,3 +1,4 @@
+using Minimilist.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,116 @@ namespace Minimilist.Enemies
 {
     public class Enemy : MonoBehaviour
     {
-        // Just to detect enemy for now
+        [SerializeField] private Transform player;
+        [SerializeField] private PatrolStates state;
+        [SerializeField] private float chaseSpeed = 2;
+
+        [Header("Patrol")]
+        [SerializeField] private Transform startPoint;
+        [SerializeField] private Transform endPoint;
+
+        [Header("Detection")]
+        [SerializeField] private EnemyDetection enemyDetection;
+
+        private ObjectMovement patrolling;
+        [SerializeField] private Vector2 lastPlayerPos = Vector2.zero;
+        private Rigidbody2D rb;
+
+        private enum PatrolStates
+        {
+            Idle,
+            Chase,
+            Alert,
+            Statue,
+            Patrol,
+        }
+
+
+        private void Awake()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            patrolling = GetComponent<ObjectMovement>();
+            rb = GetComponent<Rigidbody2D>();
+            if (enemyDetection == null)
+                enemyDetection = GetComponentInChildren<EnemyDetection>();
+        }
+
+        private void Start()
+        {
+            state = PatrolStates.Idle;
+        }
+
+        private void Update()
+        {
+            if (enemyDetection.HasDetected && state != PatrolStates.Chase)
+                state = PatrolStates.Chase;
+            else if(!enemyDetection.HasDetected && state == PatrolStates.Chase)
+                state = PatrolStates.Alert;
+        }
+
+        private void FixedUpdate()
+        {
+            switch (state)
+            {
+                case PatrolStates.Idle:     HandleIdleState();  break;
+                case PatrolStates.Chase:    HandleChaseState(); break;
+                case PatrolStates.Alert:    HandleAlertState(); break;
+                case PatrolStates.Patrol:   HandlePatrolState(); break;
+                case PatrolStates.Statue:
+                    // Statue
+                    // Dont do anything
+                    break;
+                default: Debug.LogError($"Something went wrong! {state} is not implemented!"); break;
+            }
+
+            void HandleIdleState()
+            {
+                patrolling.StopMoving();
+                StartCoroutine(ChangeState(PatrolStates.Patrol, 10));
+            }
+
+            void HandleChaseState()
+            {
+                StopAllCoroutines();
+                patrolling.StopMoving();
+                var dir = player.position - transform.position;
+                dir.Normalize();
+
+                var xScale = Mathf.Abs(transform.localScale.x);
+                transform.localScale = new Vector2(dir.x < 0 ? -xScale : xScale, transform.localScale.y);
+
+                Vector2 targetPos = new Vector2(dir.x < 0 ? -chaseSpeed : chaseSpeed, rb.velocity.y);
+                rb.velocity = Vector2.Lerp(rb.velocity, targetPos, Time.fixedDeltaTime * chaseSpeed);
+                lastPlayerPos = player.position;
+                lastPlayerPos.y = transform.position.y;
+            }
+
+            void HandleAlertState()
+            {
+                var dir = (Vector3)lastPlayerPos - transform.position;
+                dir.Normalize();
+
+                Vector2 targetPos = new Vector2(dir.x < 0 ? -chaseSpeed : chaseSpeed, rb.velocity.y);
+                if (Mathf.Abs(lastPlayerPos.x - transform.position.x) > 0)
+                    rb.velocity = Vector2.Lerp(rb.velocity, targetPos, Time.fixedDeltaTime * chaseSpeed / 2);
+                else
+                    rb.velocity = Vector2.zero;
+
+                StartCoroutine(ChangeState(PatrolStates.Patrol, 5f));
+            }
+
+            void HandlePatrolState()
+            {
+                patrolling.StartMoving();
+                StartCoroutine(ChangeState(PatrolStates.Idle, 20));
+            }
+
+            IEnumerator ChangeState(PatrolStates newState, float changeTime)
+            {
+                yield return new WaitForSeconds(changeTime);
+                StopAllCoroutines();
+                state = newState;
+            }
+        }
     }
 }
--- Inverse_Unity/Assets/Tests/Jaydeep/Scripts/Utilities/ObjectMovement.cs
+++ Inverse_Unity/Assets/Tests/Jaydeep/Scripts/Utilities/ObjectMovement.cs
@@ -6,13 +6,17 @@ namespace Minimilist.Utilities
 {
     public class ObjectMovement : MonoBehaviour
     {
+        [SerializeField] private bool flip;
         [SerializeField] private float speed = 0f;
         [SerializeField] private Transform startPos = null;
         [SerializeField] private Transform targetPos = null;
         private bool isReached = false;
+        private bool isMoving = true;
 
         private void FixedUpdate()
         {
+            if (!isMoving) return;
+
             if (Vector3.Distance(transform.position, startPos.position) < .5f)
                 isReached = false;
             else if (Vector3.Distance(transform.position, targetPos.position) < .5f)
@@ -20,7 +24,12 @@ namespace Minimilist.Utilities
 
             var target = isReached ? startPos : targetPos;
 
+            var xScale = Mathf.Abs(transform.localScale.x);
+            transform.localScale = new Vector2(isReached ? -xScale : xScale, transform.localScale.y);
             transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
         }
+
+        public void StartMoving() { isMoving = true; }
+        public void StopMoving() {  isMoving = false; }
     }
 }
