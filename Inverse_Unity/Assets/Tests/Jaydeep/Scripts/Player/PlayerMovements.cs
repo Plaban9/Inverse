25c68e8b2df664c19dee72551c889bc86e519c23
Filipe <51495797+fmmariz@users.noreply.github.com>
2024-03-18 09:10:50 -0400

    Snappy jump

--- Inverse_Unity/Assets/Tests/Jaydeep/Scripts/Player/PlayerMovements.cs
+++ Inverse_Unity/Assets/Tests/Jaydeep/Scripts/Player/PlayerMovements.cs
@@ -18,6 +18,13 @@ namespace Minimalist.Player
         [SerializeField] private Transform groundCheck;
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private float groundCheckRadius = .5f;
+        [SerializeField] private int airJumpCap = 1;
+        [SerializeField] private float airControl = 0.7f;
+        private int _airJumpCount = 0;
+
+        private float fallMultiplier = 2.5f;
+        private float lowJumpMultiplier = 2f;
+
 
         public Vector2 Velocity { get => _rb.velocity; }
         public bool IsGrounded { get => isGrounded; }
@@ -35,17 +42,36 @@ namespace Minimalist.Player
         private void Update()
         {
             float move = _playerInput.MoveVector;
+            float finalSpeed = speed;
+            if(!isGrounded)
+            {
+                finalSpeed *= airControl;
+            }
 
-            Vector2 myVelocity = new Vector2(move * speed, _rb.velocity.y);
+            Vector2 myVelocity = new Vector2(move * finalSpeed, _rb.velocity.y);
 
             //_rb.velocity = myVelocity;
-            _rb.velocity = Vector2.Lerp(_rb.velocity, myVelocity, Time.deltaTime * speed);
+            _rb.velocity = myVelocity; //Vector2.Lerp(_rb.velocity, myVelocity, Time.deltaTime * speed);
 
             isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-            if(isGrounded && _playerInput.IsJumped)
+            if (isGrounded)
             {
+                _airJumpCount = 0;
+            }
+
+            if((isGrounded || airJumpCap > _airJumpCount) && _playerInput.IsJumped)
+            {
+                if (!isGrounded) { _airJumpCount++; }
                 _rb.velocity = new Vector2(_rb.velocity.x, jumpHeight);
+            }
+
+            if(_rb.velocity.y < 0)
+            {
+                _rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            }else if(_rb.velocity.y > 0 && !_playerInput.IsJumped)
+            {
+                _rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
             }
         }
 
