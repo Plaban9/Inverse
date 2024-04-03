50d0a1fc1630800169af8b2c0fe0d6f8a7480f55
Plaban Biswas <plabanbiswas96@outlook.com>
2024-04-02 21:52:17 -0400

    - ADDED: Boolean support for Save System

--- Inverse_Unity/Assets/Scripts/SaveSystem/SaveManager.cs
+++ Inverse_Unity/Assets/Scripts/SaveSystem/SaveManager.cs
@@ -12,7 +12,7 @@ namespace Minimalist.SaveSystem
         #region Read Methods
         public static string ReadData(string key, string defaultValue = "")
         {
-            d($"Reading Data: Key: {key}, Deafult Value: {defaultValue}");
+            D($"Reading Data: Key: {key}, Deafult Value: {defaultValue}");
             return PlayerPrefs.GetString(key, defaultValue);
         }
 
@@ -25,13 +25,23 @@ namespace Minimalist.SaveSystem
         {
             return int.TryParse(ReadData(key, defaultValue.ToString()), out int result) ? result : defaultValue;
         }
+
+        public static bool ReadData(string key, bool defaultValue = false)
+        {
+            return bool.TryParse(ReadData(key, defaultValue.ToString()), out bool result) ? result : defaultValue;
+        }
         #endregion
 
         #region Save Methods
         public static void SaveData(string key, string value)
         {
-            d($"Saving Data: Key: {key}, Value: {value}");
+            D($"Saving Data: Key: {key}, Value: {value}");
             PlayerPrefs.SetString(key, value.ToString());
+        }
+
+        public static void SaveData(string key, bool value)
+        {
+            SaveData(key, value.ToString());
         }
 
         public static void SaveData(string key, int value)
@@ -45,7 +55,7 @@ namespace Minimalist.SaveSystem
         }
         #endregion
 
-        private static void d(string message)
+        private static void D(string message)
         {
             //Debug.Log("<<SaveManager>> " + message);
         }
